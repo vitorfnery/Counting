ee46543b9dc2572bfd6bdc05b51ecad39d252e95
Vítor F. Nery <101281929+vitorfnery@users.noreply.github.com>
2022-05-31 22:19:03 -0300

    initial commit
    Counting Page's first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function contar() {
+  let inicio = document.getElementById('txtinicio')
+  let fim = document.getElementById('txtfim')
+  let passo = document.getElementById('txtpasso')
+  let res = document.getElementById('res')
+
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    window.alert('ERRO: Verifique os dados e tente novamente.')
+  } else {
+    res.innerHTML = 'Contando: <br>'
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+
+    if (p <= 0) {
+      window.alert('Passo inválido! Considerando passo = 1')
+      p = 1
+    }
+
+    if (i < f) {
+      // Contagem crescente
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}` // += é concatenação nesse caso
+      }
+    } else {
+      // Contagem regressiva
+      for (let c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`
+      }
+    }
+  }
+  res.innerHTML += `\u{1F3C1}`
+}
